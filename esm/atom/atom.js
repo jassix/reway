e1306bc816c59057ae33eefd19dfc270bb7482e1
jassix <jassix@pm.me>
2023-05-10 12:50:29 +0300

    modules build

--- esm/atom/atom.js
+++ esm/atom/atom.js
@@ -2,8 +2,9 @@ let atomCount = 0;
 export const atom = ((...[state]) => {
     let currentState = state;
     let listeners = {};
+    let activeModules = {};
     const atomIndex = `atom_${++atomCount}`;
-    return {
+    const methods = {
         get: (selected) => {
             if (!selected) {
                 return currentState;
@@ -26,6 +27,20 @@ export const atom = ((...[state]) => {
             else {
                 currentState = selected;
             }
+            Object.keys(activeModules).map((item) => {
+                activeModules[item].map((module) => {
+                    if (item == "all") {
+                        module(currentState);
+                    }
+                    if (typeof selected == 'object') {
+                        Object.keys(selected).map((selectedItem) => {
+                            if (selectedItem == item) {
+                                module(currentState);
+                            }
+                        });
+                    }
+                });
+            });
             Object.keys(listeners).map((item) => {
                 if (item == "data") {
                     listeners[item].map((callback) => {
@@ -44,7 +59,31 @@ export const atom = ((...[state]) => {
                 listeners[method] = [];
             }
             listeners[method].push(callback);
+        },
+        append: (...modules) => {
+            modules.map((item) => {
+                if (!Array.isArray(item.affected)) {
+                    if (!Array.isArray(activeModules["all"])) {
+                        activeModules["all"] = [];
+                    }
+                    activeModules["all"].push(item.action);
+                }
+                else {
+                    item.affected.map((affectedItem) => {
+                        if (!Array.isArray(activeModules[affectedItem])) {
+                            activeModules[affectedItem] = [];
+                        }
+                        activeModules[affectedItem].push(item.action);
+                    });
+                }
+            });
+            return {
+                on: methods.on,
+                set: methods.set,
+                get: methods.get,
+            };
         }
     };
+    return methods;
 });
 //# sourceMappingURL=atom.js.map
--- esm/atom/atom.types.d.ts
+++ esm/atom/atom.types.d.ts
@@ -5,10 +5,15 @@ export type AtomSetSelectCheck<InitialState> = InitialState extends object ? Ato
 export type AtomOnMethod<InitialState, Key> = InitialState extends object ? Key : "data";
 export type AtomOnCallback<InitialState, Key extends keyof InitialState> = InitialState extends object ? InitialState[Key] : InitialState;
 export type AtomListeners<InitialState> = Record<string, Array<(<Key extends keyof InitialState>(state: AtomOnCallback<InitialState, Key>) => any)>>;
+export type AtomModule<InitialState> = {
+    affected: InitialState extends object ? Array<keyof InitialState> | "all" : void;
+    action: (state: InitialState) => void;
+};
 export type Atom<InitialState> = {
     get: (selected?: AtomGetSelectCheck<InitialState>) => Partial<Readonly<InitialState>>;
     set: (selected?: AtomSetSelectCheck<InitialState>) => any;
     on: <Key extends keyof InitialState>(method: AtomOnMethod<InitialState, Key>, callback: (state: AtomOnCallback<InitialState, Key>) => any) => any;
+    append: (...modules: AtomModule<InitialState>[]) => Omit<Atom<InitialState>, 'append'>;
 };
 export type CreateAtomParameters<InitialState> = Parameters<(state: InitialState) => Atom<InitialState>>;
 //# sourceMappingURL=atom.types.d.ts.map
